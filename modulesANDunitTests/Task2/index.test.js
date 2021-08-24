7983448835cf84007ef1664ff45aab7359bcb65a
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-08-24 19:27:41 +0300

    name

--- modulesANDunitTests/Task2/index.test.js
+++ modulesANDunitTests/Task2/index.test.js
@@ -1,6 +1,6 @@
-import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.test.js';
+import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.js';
 it('shold get squared numvers', () => {
-    const result = getSquaredNumbers([1, 2, 3]);
+    const result = getSquaredArray([1, 2, 3]);
 
     expect(result).toEqual([1, 4, 9]);
 })
