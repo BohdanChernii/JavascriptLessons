16644457b05b60ed522b268c7cba9ca6d442434d
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-08-24 21:13:02 +0300

    qwrwre

--- modulesANDunitTests/Task2/calculator.js
+++ modulesANDunitTests/Task2/calculator.js
@@ -1,6 +1,15 @@
-export const getSquaredArray = arr =>
-    arr.map(num => num * num);
-export const getOddNumbers = arr =>
-    arr.filter(num => (num % 2 === 1));
+import getSum, { getSquaredArray, getOddNumbers } from './index.item.js';
+it('shold get squared numbers', () => {
+    const result = getSquaredArray([1, 2, 3]);
+    expect(result).toEqual([1, 4, 9]);
+})
 
-export default (a, b) => a + b;
+it('shold keep odd numbers only', () => {
+    const result = getOddNumbers([1, 2, 3, 4, 5]);
+    expect(result).toEqual([1, 3, 5]);
+})
+
+it('shold get sum of numbers ', () => {
+    const result = getSum(8, 4);
+    expect(result).toEqual(12);
+})
