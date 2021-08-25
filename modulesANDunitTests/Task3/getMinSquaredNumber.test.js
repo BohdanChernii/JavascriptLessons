00ac8afe358c6e7866b642d40d21ffb53c94c00b
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-08-25 15:56:23 +0300

    5ttr

--- modulesANDunitTests/Task3/getMinSquaredNumber.test.js
+++ modulesANDunitTests/Task3/getMinSquaredNumber.test.js
@@ -4,3 +4,13 @@ it('should get min squared element', () => {
     const result = getMinSquaredNumber([10, -2, 4, 5, 10, ])
     expect(result).toEqual(4);
 });
+
+it('should get min squared element', () => {
+    const result = getMinSquaredNumber([10, -2, 1, 5, 10, ])
+    expect(result).toEqual(1);
+});
+
+it('should get min squared element', () => {
+    const result = getMinSquaredNumber([10, -20, 0, 5, 10, ])
+    expect(result).toEqual(0);
+});
