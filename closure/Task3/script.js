733e1f986f5fb6c2bc2236b1bd3a454805841cab
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-02 14:07:50 +0300

    www

--- closure/Task3/script.js
+++ closure/Task3/script.js
@@ -7,56 +7,75 @@
 //створити метод getRecords який буде працювати з рештою методів
 //задати сортування
 const createLogger = () => {
-
-let arr = [];
+  let arr = [];
 
   function log(str) {
     arr.push({
       message: str,
       dateTime: new Date(),
-      type: 'log',
-    })
+      type: "log",
+    });
   }
   function warn(str) {
     arr.push({
       message: str,
       dateTime: new Date(),
-      type: 'warn',
-    })
+      type: "warn",
+    });
   }
   function error(str) {
     arr.push({
       message: str,
       dateTime: new Date(),
-      type: 'error',
-    })
+      type: "error",
+    });
   }
-  
 
   //має приймати тип логера
   //і якшо співпадає з типом обєrта пушити в res
-  //якшо аргументи не задані вивести всі обєкти 
+  //якшо аргументи не задані вивести всі обєкти
   //потім методом sort b-a по даті
-
-  function getRecord() {
- arr.sort((a,b) => a.dateTime - b.dateTime);
-   return arr
-  }
+  
+  function getRecords(type) {
+    
+    if (type) {
+     let res = arr.sort((a, b) => b.dateTime - a.dateTime).filter((el) => el.type === type);
+     return res
+    } 
+      return arr.sort((a, b) =>{ 
+        return b.dateTime - a.dateTime}) 
+    }
+  
 
   return {
     log,
     warn,
     error,
-    getRecord,
+    getRecords,
   };
-
 };
 const logger = createLogger();
-logger.getRecord();
+logger.getRecords();
+
+
+logger.error("Uexpected error on the site");
 logger.log("User logged in");
-logger.warn("User try restricted on the site");
+
 logger.error("Uexpected error on the site");
 logger.error("Uexpected error on the site");
-logger.log("User logged in");
-console.log(logger.getRecord());
+logger.error("Uexpected error on the site");
+
+console.log(logger.getRecords(''));
+const logger2 = createLogger();
+logger2.getRecords();
+
+logger2.warn("User try restricted on the site");
+logger2.error("Uexpected error on the site");
+logger2.log("User logged in");
+logger2.warn("User try restricted on the site");
+logger2.error("Uexpected error on the site");
+logger2.error("Uexpected error on the site");
+logger2.error("Uexpected error on the site");
+
+console.log(logger2.getRecords());
 
