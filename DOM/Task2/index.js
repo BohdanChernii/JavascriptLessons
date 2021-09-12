b6ce9cfc2e3827c91494e00102205a3e1c670bf0
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-12 19:57:37 +0300

    bb

--- DOM/Task2/index.js
+++ DOM/Task2/index.js
@@ -1,13 +1,13 @@
 export const getTitleElement = () => {
     const text = document.querySelector('.title');
     console.dir(text.outerHTML)
-    return text.outerHTML;
+    return text;
 }
 getTitleElement()
 export const getInputElement = () =>{
     const input = document.querySelector('input');
     input.getAttribute('type','text');
     console.dir(input.outerHTML)
-    return input.outerHTML
+    return input
 }
 getInputElement()
