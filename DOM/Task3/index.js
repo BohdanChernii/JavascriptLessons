17ad3cbea44bc696f8c373469c65670b5a70d51e
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-12 20:39:30 +0300

    rtrt

---
+++ DOM/Task3/index.js
@@ -0,0 +1,14 @@
+export const getItemsList = () => {
+    const technology = document.querySelectorAll('.technology')
+    console.dir(technology)
+    return technology
+
+}
+getItemsList()
+export const getItemsArray = () => {
+    const tool = document.querySelectorAll('.tool')
+    console.dir(Array.from(tool))
+    return tool
+}
+getItemsArray()
+
