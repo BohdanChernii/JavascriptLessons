1bac141f95f2b07cb179008ac0f269be36235323
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-13 15:29:16 +0300

    kg

--- DOM/Task10/index.js
+++ DOM/Task10/index.js
@@ -2,9 +2,9 @@ export const finishForm = () => {
   const form = document.querySelector(".login-form");
   const input = document.createElement("input");
 input.setAttribute('name','login')
+input.setAttribute('type','text')
 
   form.prepend(input);
    let last = Array.from( document.querySelectorAll('input')).pop();
    last.setAttribute('type','password');
 };
-
