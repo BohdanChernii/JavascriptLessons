e3607669114c548ed2e6438e5da96fa78271b37f
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-13 15:55:10 +0300

    212w

---
+++ DOM/Task11/index.js
@@ -0,0 +1,8 @@
+export const manageClasses = () => {
+  const items = Array.from(document.querySelectorAll('li'))
+  items[0].classList.toggle('selected');
+  items[1].classList.toggle('selected');
+  items[2].classList.toggle('three_done')
+  items[3].classList.toggle('another_class','some-class')
+}
+manageClasses()
