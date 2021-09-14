4e9745aec7cab4f2853dc3c73ddcb94d4a2f4f04
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-14 16:51:06 +0300

    wwdwdw

--- Events/Task7/index.js
+++ Events/Task7/index.js
@@ -1,6 +1,8 @@
-
-const body = document.querySelector('body')
+const buttons = Array.from(document.querySelectorAll('.btn'));
+const firstButton = buttons[0];
+const secondButton = buttons[1];
 function handleClick(event) {
   console.log(event.target.textContent);
 }
-body.addEventListener("click", handleClick);
+firstButton.addEventListener("click", handleClick);
+secondButton.addEventListener("click", handleClick);
