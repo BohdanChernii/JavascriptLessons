c5a044b6ec1853f5e049a1a4527c68e44880b242
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-14 14:02:12 +0300

    ефіл

---
+++ Events/Task2/index.js
@@ -0,0 +1,5 @@
+export const button = document.querySelector('.create-btn');
+function createAlert (){
+    alert('done')
+}
+button.addEventListener('click', createAlert) //1 прийме назву івента 2 прийме силку на  функцію 
