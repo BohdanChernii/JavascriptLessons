4067d0d9a855b44053b5f8d5fcdfe3b9dc3dab64
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-15 23:19:31 +0300

    mklml;kml;

--- DOM/Task1/index.js
+++ DOM/Task1/index.js
@@ -1,41 +1,58 @@
-const listElem = document.querySelector('.list');
-const taskInput = document.querySelector('.task-input')
-const button = document.querySelector('.create-task-btn')
- const tasks = [
-  { text: "Buy milk", done: false },
-  { text: "Pick up Tom from airport", done: false },
-  { text: "Visit party", done: false },
-  { text: "Visit doctor", done: true },
-  { text: "Buy meat", done: true },
+const listElem = document.querySelector(".list");
+const taskInput = document.querySelector(".task-input");
+const button = document.querySelector(".create-task-btn");
+const tasks = [
+  { text: "Buy milk", done: false, id: 1 },
+  { text: "Pick up Tom from airport", done: false, id: 2 },
+  { text: "Visit party", done: false, id: 3 },
+  { text: "Visit doctor", done: true, id: 4 },
+  { text: "Buy meat", done: true, id: 5 },
 ];
 
+const generateId = () => Math.random().toFixed(4) * 10000;
 const addNewEL = () => {
-  tasks.push({ text:taskInput.value, done: false })
-  renderTasks(tasks)
-}
-button.addEventListener('click',addNewEL)
+  tasks.push({ text: taskInput.value, done: false, id: generateId() });
+  renderTasks(tasks);
+};
+button.addEventListener("click", addNewEL);
 
 const checkCheckbox = (event) => {
-if(!event.target.classList.contains('list__item-checkbox')){
-return false
-}  
-}
-listElem.addEventListener('click', checkCheckbox)
+  const item = event.target;
 
-const renderTasks = tasksList => {
-  listElem.innerHTML = '';
+  if (!item.classList.contains("list__item-checkbox")) {
+    return false;
+  }
+
+  const itemId = item.closest("li").dataset.id; // here we getting id of li as string
+
+  tasks.map((el) => {
+    if (el.id === parseInt(itemId)) {
+      // here we parsing itemId because it is string and we need number
+      el.done = true; // changing field done in object
+    }
+  });
+
+  renderTasks(tasks); // re rendering updated list
+};
+
+listElem.addEventListener("click", checkCheckbox);
+
+const renderTasks = (tasksList) => {
+  listElem.innerHTML = "";
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement("li");
+      listItemElem.classList.add("list__item");
+      listItemElem.dataset.id = id;
+
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
 
       checkbox.checked = done;
-      checkbox.classList.add('list__item-checkbox');
+      checkbox.classList.add("list__item-checkbox");
       if (done) {
-        listItemElem.classList.add('list__item_done');
+        listItemElem.classList.add("list__item_done");
       }
       listItemElem.append(checkbox, text);
 
@@ -46,7 +63,3 @@ const renderTasks = tasksList => {
 };
 
 renderTasks(tasks);
-
-
-
- 
