9fb12efc692166c3715cb96dcabc9f944ade8081
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-16 16:06:28 +0300

    axcaca

---
+++ Dates/Task1/index.js
@@ -0,0 +1,8 @@
+const weeekDays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "St"];
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const futureDay = new Date(date).setDate(day + days);
+  return weeekDays[new Date(futureDay).getDay()];
+};
+const result = dayOfWeek(new Date(2021, 9, 15), 1);
+console.log(result);
