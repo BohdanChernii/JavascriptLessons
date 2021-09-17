ea236717b7ab930c9ccf352e9993033609d39766
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-17 15:32:31 +0300

    dadad

--- Browser(local)Storage/Task1/index.js
+++ Browser(local)Storage/Task1/index.js
@@ -1,13 +1,12 @@
-localStorage.setItem('Name', JSON.stringify({name:'Bohdan'}))
-localStorage.setItem('Age', JSON.stringify({age:20}))
-localStorage.setItem('Smaking', JSON.stringify({smaking:false}))
-localStorage.setItem('Capital', JSON.stringify({capital:null}))
+localStorage.setItem("Name", JSON.stringify({ name: "Bohdan" }));
+localStorage.setItem("Age", JSON.stringify({ age: 20 }));
+localStorage.setItem("Smaking", JSON.stringify({ smaking: false }));
+localStorage.setItem("Capital", JSON.stringify({ capital: null }));
 
- export const getLocalStorageData = () => {
-return Object.entries(localStorage).reduce((acc, [key, value])=>{
-     
-    return {...acc,[key]: JSON.parse(value),}
-},{})
- }
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    return { ...acc, [key]: JSON.parse(value) };
+  }, {});
+};
 
- getLocalStorageData()
+getLocalStorageData();
