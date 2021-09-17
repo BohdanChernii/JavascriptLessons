c8220ee93452b156c38000ad986f9c69727b7c51
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-17 15:28:38 +0300

    wqdq

--- Browser(local)Storage/Task1/index.js
+++ Browser(local)Storage/Task1/index.js
@@ -2,14 +2,11 @@ localStorage.setItem('Name', JSON.stringify({name:'Bohdan'}))
 localStorage.setItem('Age', JSON.stringify({age:20}))
 localStorage.setItem('Smaking', JSON.stringify({smaking:false}))
 localStorage.setItem('Capital', JSON.stringify({capital:null}))
-console.log(JSON.parse(localStorage.getItem('Name')))
-console.log(JSON.parse(localStorage.getItem('Age')))
-console.log(JSON.parse(localStorage.getItem('Smaking')))
-console.log(JSON.parse(localStorage.getItem('Capital')))
+
  export const getlocalStorageData = () => {
 return Object.entries(localStorage).reduce((acc, [key, value])=>{
      
     return {...acc,[key]: JSON.parse(value),}
 },{})
  }
- console.log(getlocalStorageData())
+
