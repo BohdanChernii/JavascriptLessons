a6d505152c450e04084a22a74874652f6262376f
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-26 15:09:39 +0300

    xsax

--- Browser(local)Storage/Task1/index.js
+++ Browser(local)Storage/Task1/index.js
@@ -5,12 +5,12 @@ localStorage.setItem("Capital", JSON.stringify({ capital: null }));
 
 const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    let newValue;  
-    try{
-        newValue = JSON.parse(value)
-      }catch(e){
-        newValue = value;
-      }
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
     return { ...acc, [key]: newValue };
   }, {});
 };
