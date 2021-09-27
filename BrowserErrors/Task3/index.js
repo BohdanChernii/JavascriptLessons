7f81c29eaf6750270a1ee2a92785620810ea7e72
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-09-27 15:39:00 +0300

    fdf

--- BrowserErrors/Task3/index.js
+++ BrowserErrors/Task3/index.js
@@ -1,6 +1,6 @@
-import {  fetchUserData, fetchRepositories } from "./gateways.js";
-import { renderRepos,  cleanRepoList, renderuserData } from "./repos.js";
-import { hideSpinner, showSpinner } from "./spinner.js"
+import { fetchUserData, fetchRepositories } from "./gateways.js";
+import { renderRepos, cleanRepoList, renderuserData } from "./repos.js";
+import { hideSpinner, showSpinner } from "./spinner.js";
 const defaultUserAvatar = "https://avatars3.githubusercontent.com/u10001";
 const defaultUser = {
   avatar_url: defaultUserAvatar,
@@ -8,31 +8,48 @@ const defaultUser = {
   location: "",
 };
 
-
 const showUserBtn = document.querySelector(".name-form__btn");
 const userNameInput = document.querySelector(".name-form__input");
 
 renderuserData(defaultUser);
 
-const onSearchUser = () => {
-showSpinner();
+const onSearchUser = async () => {
+  showSpinner();
+  cleanRepoList();
   const userName = userNameInput.value;
-  fetchUserData(userName)
-    .then((userData) => {
-      renderuserData(userData);
-      return userData.repos_url;
-    })
-    .then((url) => fetchRepositories(url))
-    .then((repoList) => {
-
-       renderRepos(repoList)
-       hideSpinner();
-    })
-    .catch((err) => {
-        alert(err.message)
-    })
-    .finally(()=>{
-        hideSpinner();
-    })
+  try{
+    const userData = await fetchUserData(userName);
+    renderuserData(userData);
+    const repoList = await fetchRepositories(userData.repos_url);
+    renderRepos(repoList);
+  }
+catch(err){
+alert(err.message);
+}
+finally{
+  hideSpinner();
+}
 };
+
+// const onSearchUser = () => {
+// showSpinner();
+//   const userName = userNameInput.value;
+//   fetchUserData(userName)
+//     .then((userData) => {
+//       renderuserData(userData);
+//       return userData.repos_url;
+//     })
+//     .then((url) => fetchRepositories(url))
+//     .then((repoList) => {
+
+//        renderRepos(repoList)
+//        hideSpinner();
+//     })
+//     .catch((err) => {
+//         alert(err.message)
+//     })
+//     .finally(()=>{
+//         hideSpinner();
+//     })
+// };
 showUserBtn.addEventListener("click", onSearchUser);
